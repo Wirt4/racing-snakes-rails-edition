27baf66291ac4510943ab17e2b63ca65344adfec
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-06-29 16:36:23 -0700

    updated tests

--- spec/views/landing/index.html.erb_spec.rb
+++ spec/views/landing/index.html.erb_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe "landing/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe 'landing/index.html.erb', type: :view do
+  it 'shows the sign up link' do
+    render
+    expect(rendered).to include('Play Now!')
+    expect(rendered).to include('href="/play"')
+  end
 end
