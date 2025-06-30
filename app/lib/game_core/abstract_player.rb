a9deeaf354dc1e7067bac5168d727b67f638b012
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-06-30 15:59:15 -0700

    added comment

--- app/lib/game_core/abstract_player.rb
+++ app/lib/game_core/abstract_player.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
 module RacingSnakes
-  # TODO: list invariants
+  # AbstractPlayer is an abstract class that represents a player in the game.
+  # invariants: id is always an alphanumeric string.
+  # TODO: move checking logic to this parent class
   class AbstractPlayer
     attr_reader :id
   end
