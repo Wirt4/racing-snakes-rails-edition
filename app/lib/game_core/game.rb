4643ef44a0a8479492eca25a09273db5b4e0bb26
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-06-30 15:08:07 -0700

    green

--- app/lib/game_core/game.rb
+++ app/lib/game_core/game.rb
@@ -14,7 +14,9 @@ module RacingSnakes
       # preconditions: player_id is a 32 character string
       # player is not already in the game
       # a player is created with the player name (player_id)
+      @players ||= []
       validate_player_id(player_id)
+      @players << player_id
 
       # postconditions: player count is incremented by 1s a player is added which has the name
     end
@@ -24,6 +26,7 @@ module RacingSnakes
     def validate_player_id(player_id)
       raise ArgumentError, 'player_id must be a string' unless player_id.is_a?(String)
       raise ArgumentError, 'player_id must be a 32 character hex string' unless player_id.match?(/\A[a-f0-9]{32}\z/)
+      raise ArgumentError, 'player_id already exists' if @players&.include?(player_id)
     end
   end
 end
