10a950cc5e0b11a7a05167419ce5c5b9fda9f8a5
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-06-30 15:57:01 -0700

    added top level comment

--- app/lib/player/player_factory.rb
+++ app/lib/player/player_factory.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module RacingSnakes
+  # Factory class to create player instances
   class PlayerFactory
     def self.build(player_id)
       RacingSnakes::HumanPlayer.new(player_id: player_id)
