739ba8d5aa3e7edbd43e589a1cc7718129a6efc2
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-06-30 15:04:27 -0700

    red

--- spec/lib/game_spec.rb
+++ spec/lib/game_spec.rb
@@ -4,8 +4,10 @@ require_relative '../../app/lib/racing_snakes'
 RSpec.describe RacingSnakes::Game do
   describe '#add_player' do
     let(:game) { described_class.new }
+    player_id = 'a2fc0b19dfea4c278379c8d9b79a4f6b'
+
     it 'game has an add_player method' do
-      game.add_player('a2fc0b19dfea4c278379c8d9b79a4f6b')
+      game.add_player(player_id)
     end
     it 'the player id must be a string' do
       expect do
@@ -17,6 +19,12 @@ RSpec.describe RacingSnakes::Game do
       expect do
         game.add_player(' this is not a valid player id')
       end.to raise_error(ArgumentError, /player_id must be a 32 character hex string/)
+    end
+    it 'player_ids may not be duplicates' do
+      game.add_player(player_id)
+      expect do
+        game.add_player(player_id)
+      end.to raise_error(ArgumentError, /player_id already exists/)
     end
   end
 end
