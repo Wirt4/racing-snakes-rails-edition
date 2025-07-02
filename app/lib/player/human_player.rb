6ab7b7217fd7c4b3a44c0b3d9b1c5385148f9078
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-07-01 21:33:47 -0700

    removed unnecessary work

--- app/lib/player/human_player.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module RacingSnakes
-  # TODO: list invariants
-  class HumanPlayer < RacingSnakes::AbstractPlayer
-  end
-end
--- app/lib/racing_snakes.rb
+++ app/lib/racing_snakes.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
 require_relative 'game_core/game'
-require_relative 'game_core/abstract_player'
 require_relative 'game_core/abstract_board'
 require_relative 'player/player_factory'
-require_relative 'player/human_player'
 require_relative 'game_core/abstract_player_roster'
 require_relative 'player/player_roster_factory'
 # add other namespaced files here
