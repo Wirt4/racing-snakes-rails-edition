4178724d9a05e3703e22507c062ffa5486a34f5e
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-07-03 18:10:04 -0700

    another CI test update

--- app/lib/racing_snakes.rb
+++ app/lib/racing_snakes.rb
@@ -3,7 +3,7 @@
 require_relative 'racing_snakes/game'
 require_relative 'racing_snakes/abstract_board'
 require_relative 'racing_snakes/abstract_player_roster'
-require_relative 'player/player_roster'
+require_relative 'racing_snakes/player_roster'
 
 # add other namespaced files here
 module RacingSnakes
