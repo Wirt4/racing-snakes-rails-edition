43d952a6df177038043f5b7b73e308f6f413bce4
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-07-05 11:57:24 -0700

    added colors

--- app/raycaster/src/main.ts
+++ app/raycaster/src/main.ts
@@ -1,3 +1,4 @@
+//set up the canvas
 // assumes target will have an empty div tag with id "app"
 const app = document.getElementById("app");
 const canv = document.createElement("canvas");
@@ -6,3 +7,29 @@ const square_size = 300;
 canv.width = square_size;
 canv.height = square_size;
 app?.appendChild(canv);
+
+//grab the canvas and add a rectangle to it
+const canvas = document.getElementById("my-house") as HTMLCanvasElement;
+const ctx = canvas.getContext("2d");
+if (!ctx) {
+	throw new Error("Failed to get canvas context");
+}
+ctx.lineWidth = 10;
+
+// Wall
+// assets in the CANVAS API are defaulted to black
+ctx.strokeStyle = "#8B4513";
+ctx.strokeRect(75, 140, 150, 110);
+
+// Door
+ctx.fillStyle = "#654321";
+ctx.fillRect(130, 190, 40, 60);
+
+// Roof
+ctx.strokeStyle = "#A52A2A"; // reddish-brow
+ctx.beginPath();
+ctx.moveTo(50, 140);
+ctx.lineTo(150, 60);
+ctx.lineTo(250, 140);
+ctx.closePath();
+ctx.stroke();
