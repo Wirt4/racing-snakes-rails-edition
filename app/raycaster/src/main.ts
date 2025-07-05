7e66f71682527fc3b3bc1342626f19c88a5034f8
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-07-05 12:46:06 -0700

    remove duplication

--- app/raycaster/src/main.ts
+++ app/raycaster/src/main.ts
@@ -9,31 +9,18 @@ canv.height = square_size;
 app?.appendChild(canv);
 
 //grab the canvas and add a rectangle to it
-const canvas = document.getElementById("my-house") as HTMLCanvasElement;
+const canvas = document.getElementById(canv.id) as HTMLCanvasElement;
 const ctx = canvas.getContext("2d");
 if (!ctx) {
 	throw new Error("Failed to get canvas context");
 }
-ctx.translate(150, 150);
+const center_xy: number = square_size / 2;
+ctx.translate(center_xy, center_xy); // translate to center of canvas
 ctx.rotate((45 * Math.PI) / 180); //experiment with rotation
-ctx.translate(-150, -150); // reset translation to origin
+ctx.translate(-center_xy, -center_xy); // reset translation to origin
 
-ctx.lineWidth = 10;
 
-// Wall
-// assets in the CANVAS API are defaulted to black
-ctx.strokeStyle = "#8B4513";
-ctx.strokeRect(75, 140, 150, 110);
 
 // Door
-ctx.fillStyle = "#654321";
+ctx.fillStyle = "#AAFF00"; // a nice green
 ctx.fillRect(130, 190, 40, 60);
-
-// Roof
-ctx.strokeStyle = "#A52A2A"; // reddish-brow
-ctx.beginPath();
-ctx.moveTo(50, 140);
-ctx.lineTo(150, 60);
-ctx.lineTo(250, 140);
-ctx.closePath();
-ctx.stroke();
