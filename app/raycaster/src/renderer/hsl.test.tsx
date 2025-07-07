7a4ea5a8c3afaf20f50b690dea427ed44a32cc3e
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-07-06 23:22:02 -0700

    green

--- app/raycaster/src/renderer/hsl.test.tsx
+++ app/raycaster/src/renderer/hsl.test.tsx
@@ -2,8 +2,14 @@ import { describe, test, expect } from '@jest/globals';
 import { HSL } from './hsl';
 describe('HSL object tests', () => {
 	test('object creation', () => {
-		const hsl = new HSL();
-		expect(hsl.hue).toBeDefined();
+		const hsl = new HSL(240.5);
+		expect(hsl.hue).toEqual(240.5);
+	})
+	test('enforce validity of hue', () => {
+		expect(() => new HSL(-10)).toThrow("Hue must be between 0 and 360");
+		expect(() => new HSL(370)).toThrow("Hue must be between 0 and 360");
+		expect(() => new HSL(360)).not.toThrow();
+		expect(() => new HSL(0)).not.toThrow();
 	})
 
 })
--- app/raycaster/src/renderer/hsl.ts
+++ app/raycaster/src/renderer/hsl.ts
@@ -1,4 +1,13 @@
 class HSL {
-	hue: number = 0;
+	hue: number;
+	constructor(hue: number) {
+		/**
+		 * Precondition: hue is a float representing the hue in degrees  between 0 and 360 (inclusive).
+			* **/
+		if (hue < 0 || hue > 360) {
+			throw new Error("Hue must be between 0 and 360");
+		}
+		this.hue = hue;
+	}
 }
 export { HSL };
