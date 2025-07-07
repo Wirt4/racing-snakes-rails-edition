153c020561c2366373c258f8fa6e4604af8f4996
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-07-06 22:38:03 -0700

    green

--- Makefile
+++ Makefile
@@ -2,11 +2,12 @@
 
 .PHONY: test lint ci
 
-test:
+ruby-test:
 	bundle exec rspec --require ./spec/support/quickfix_formatter.rb --format QuickfixFormatter
 
-
-lint::
+ts-test:
+	cd app/raycaster && npx jest
+lint:
 	bundle exec rubocop
 
 format:
--- app/raycaster/src/geometry/angle.test.tsx
+++ app/raycaster/src/geometry/angle.test.tsx
@@ -2,7 +2,8 @@ import { Angle } from './angle';
 import { describe, test, expect } from '@jest/globals';
 
 describe('Angle', () => {
-	test('test the workflow', () => {
-		expect(true).toBe(false);
+	test('defaults to a measurement in radians', () => {
+		const r = Math.PI / 2;
+		expect(new Angle(r).radians).toBe(r);
 	})
 })
