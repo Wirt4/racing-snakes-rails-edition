5c05e7dba5507929efd5ba9ce735420314018594
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-07-06 23:30:03 -0700

    red

--- app/raycaster/src/renderer/hsl.test.tsx
+++ app/raycaster/src/renderer/hsl.test.tsx
@@ -2,9 +2,10 @@ import { describe, test, expect } from '@jest/globals';
 import { HSL } from './hsl';
 describe('HSL object tests', () => {
 	test('object creation', () => {
-		const hsl = new HSL(240.5, 0.57);
+		const hsl = new HSL(240.5, 0.57, 1.0);
 		expect(hsl.hue).toEqual(240.5);
 		expect(hsl.saturation).toEqual(0.57);
+		expect(hsl.lightness).toEqual(1.0);
 	})
 	test('enforce validity of hue', () => {
 		expect(() => new HSL(-10)).toThrow("Hue must be between 0 and 360");
