20adceb78ee20efb1cb61941d712b42944fe169d
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-07-06 23:28:11 -0700

    green

--- app/raycaster/src/renderer/hsl.test.tsx
+++ app/raycaster/src/renderer/hsl.test.tsx
@@ -2,9 +2,9 @@ import { describe, test, expect } from '@jest/globals';
 import { HSL } from './hsl';
 describe('HSL object tests', () => {
 	test('object creation', () => {
-		const hsl = new HSL(240.5, 100);
+		const hsl = new HSL(240.5, 0.57);
 		expect(hsl.hue).toEqual(240.5);
-		expect(hsl.saturation).toEqual(100);
+		expect(hsl.saturation).toEqual(0.57);
 	})
 	test('enforce validity of hue', () => {
 		expect(() => new HSL(-10)).toThrow("Hue must be between 0 and 360");
@@ -12,5 +12,10 @@ describe('HSL object tests', () => {
 		expect(() => new HSL(360)).not.toThrow();
 		expect(() => new HSL(0)).not.toThrow();
 	})
-
+	test('enforce validity of saturation', () => {
+		expect(() => new HSL(240, -10)).toThrow("Saturation must be between 0 and 1");
+		expect(() => new HSL(240, 11)).toThrow("Saturation must be between 0 and 1");
+		expect(() => new HSL(240, 1)).not.toThrow();
+		expect(() => new HSL(240, 0)).not.toThrow();
+	})
 })
--- app/raycaster/src/renderer/hsl.ts
+++ app/raycaster/src/renderer/hsl.ts
@@ -3,12 +3,17 @@ class HSL {
 	saturation: number;
 	constructor(hue: number, saturation: number) {
 		/**
-		 * Precondition: hue is a float representing the hue in degrees  between 0 and 360 (inclusive).
+		 * Preconditions: 
+		 * hue is a float representing the hue in degrees  between 0 and 360 (inclusive).
+		 * saturation is a float representing the saturation as a percentage between 0 and 1 (inclusive).
 			* **/
 		if (hue < 0 || hue > 360) {
 			throw new Error("Hue must be between 0 and 360");
 		}
 		this.hue = hue;
+		if (saturation < 0 || saturation > 1) {
+			throw new Error("Saturation must be between 0 and 1");
+		}
 		this.saturation = saturation;
 	}
 }
