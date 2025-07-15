20832735a4f01cf87ed4fab9e5fe10d1a3035cd3
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-07-14 23:39:56 -0700

    top level cleanup with game module

--- app/raycaster/src/game/batchRenderer.ts
+++ app/raycaster/src/game/batchRenderer.ts
@@ -1,17 +1,196 @@
 import { ContextRendererInterface } from '../renderer/interface';
 import { ColorName } from './color/color_name';
 import { Settings } from '../settings';
+import { GameMapInterface } from '../gamemap/interface';
+import { Batches, BatchedRect } from './batches';
+import { Coordinates, LineSegment } from '../geometry/interfaces';
+import { RaycasterInterface } from '../raycaster/interface';
+import { BrightnessInterface } from '../brightness/interface';
 
 export class BatchRenderer {
+	private rays: Float32Array;
+	private batches: Batches = new Batches();
 
-	constructor(private contextRenderer: ContextRendererInterface) {
+	constructor(
+		private contextRenderer: ContextRendererInterface,
+		private raycaster: RaycasterInterface,
+		private brightness: BrightnessInterface,
+		private horizonY: number,
+		resolution: number
+	) {
+		this.rays = new Float32Array(resolution);
 	}
-	public batchSlices(): void { }
+
+	public batchSlices(gameMap: GameMapInterface): void {
+		this.batches = new Batches();
+		this.batchRenderData(gameMap);
+	}
+
 	public renderSlices(): void {
 		this.renderBackround();
+		this.renderWalls()
+		this.renderGrid();
+
 	}
+
+	public batchHUD(gameMap: GameMapInterface): void {
+		this.batches.addMapWalls(gameMap.walls);
+		this.batches.addMapWalls(gameMap.playerTrail);
+
+	}
+
+	public renderHUD(gameMap: GameMapInterface): void {
+		this.contextRenderer.save();
+		this.renderHUDMap();
+		this.renderPlayer(gameMap);
+		this.contextRenderer.restore()
+	}
+
+	private renderHUDMap(): void {
+		for (const [key, lines] of Object.entries(this.batches.mapBatches)) {
+			this.renderHUDLines(key, lines);
+		}
+	}
+	private renderHUDLines(
+		key: string,
+		lines: LineSegment[]
+	): void {
+		const { color, intensity: weight } = this.batches.unpackKey(key)
+		this.contextRenderer.stroke(color);
+		this.contextRenderer.strokeWeight(weight);
+		lines.forEach(line => this.contextRenderer.line(line));
+	}
+
+	private renderPlayer(map: GameMapInterface): void {
+		this.setPlayerContext();
+		this.contextRenderer.ellipse(map.playerPosition, 0.2);
+		this.draw2DRays(map);
+	}
+	private draw2DRays(gameMap: GameMapInterface): void {
+		this.contextRenderer.stroke(ColorName.GREEN);
+		this.contextRenderer.strokeWeight(0.05);
+		for (let index = 0; index < Settings.CANVAS_WIDTH; index++) {
+			const angle = this.rays[index];
+			const { distance } = gameMap.castRay(angle, Settings.MAX_DISTANCE);
+			const hit = this.pointOnTrajectory(gameMap.playerPosition, angle, distance);
+			this.contextRenderer.line({ start: gameMap.playerPosition, end: hit });
+		}
+	}
+	private pointOnTrajectory(coordinates: Coordinates, angle: number, distance: number): Coordinates {
+		return {
+			x: coordinates.x + Math.cos(angle) * distance,
+			y: coordinates.y + Math.sin(angle) * distance
+		};
+	}
+	private setPlayerContext(): void {
+		this.contextRenderer.stroke(ColorName.WHITE);
+		this.contextRenderer.fillColor(ColorName.RED, 100);
+		this.contextRenderer.noStroke();
+	}
+
+	private renderWalls(): void {
+		for (const [key, rects] of Object.entries(this.batches.wallBatches)) {
+			this.unpackAndRender(key, rects);
+		}
+	}
+
+	private renderGrid(): void {
+		this.contextRenderer.fillColor(ColorName.BLUE, 50);
+		this.batches.gridBatch.forEach(rectSpec => this.renderRect(rectSpec));
+	}
+
+	private unpackAndRender(
+		key: string,
+		rects: BatchedRect[],
+	): void {
+		const { color, intensity: brightnessValue } = this.batches.unpackKey(key);
+		this.contextRenderer.fillColor(color, brightnessValue);
+		this.renderRects(rects);
+	}
+
+	private renderRects(rects: BatchedRect[]): void {
+		rects.forEach(rect => this.renderRect(rect));
+	}
+
 	private renderBackround(): void {
 		this.contextRenderer.fillColor(ColorName.BLUE, 0.01);
 		this.contextRenderer.rect({ x: 0, y: 0 }, Settings.CANVAS_WIDTH, Settings.CANVAS_HEIGHT);
 	}
+
+	private renderRect(rectSpec: BatchedRect): void {
+		this.contextRenderer.rect({ x: rectSpec.x, y: rectSpec.y }, rectSpec.width, rectSpec.height);
+	}
+
+	private batchRenderData(map: GameMapInterface
+	): void {
+		this.rays = this.raycaster.getViewRays(map.playerAngle);
+		this.appendAllSlices(map);
+	}
+
+	private appendAllSlices(map: GameMapInterface): void {
+		for (let i = 0; i < this.rays.length; i++) {
+			this.appendSlice(i, map);
+		}
+	}
+
+	private appendSlice(index: number, map: GameMapInterface): void {
+		const angle = this.rays[index];
+		this.appendWallSlice(angle, index, map);
+		this.appendGridSlice(angle, index, map);
+	}
+
+	private appendWallSlice(
+		angle: number,
+		index: number,
+		map: GameMapInterface
+	): void {
+		const { distance, color } = this.getAdjustedDistance(angle, map);
+		const slice = this.sliceHeight(distance);
+		const wallBrightness = this.brightness.calculateBrightness(distance);
+		this.batches.addWallSlice(color, wallBrightness, { x: index, y: slice.origin }, slice.magnitude);
+	}
+
+	private appendGridSlice(
+		angle: number,
+		index: number,
+		map: GameMapInterface
+	): void {
+		const { gridHits } = map.castRay(angle, Settings.MAX_DISTANCE);
+		for (let j = 0; j < gridHits.length; j++) {
+			this.appendGridPoint(gridHits[j], angle, index, map);
+		}
+	}
+
+	private getAdjustedDistance(angle: number, map: GameMapInterface): { distance: number, color: ColorName } {
+		const { distance, color } = map.castRay(angle, Settings.MAX_DISTANCE);
+		const correctedDistance = this.raycaster.removeFishEye(distance, angle, map.playerAngle);
+		return { distance: correctedDistance, color };
+	}
+
+	private sliceHeight(
+		distance: number,
+	): { origin: number, magnitude: number } {
+		const wallTopOffset = Settings.WALL_HEIGHT - Settings.CAMERA_HEIGHT;
+		const wallBottomOffset = -Settings.CAMERA_HEIGHT;
+		const topY = this.horizonY - (wallTopOffset * this.raycaster.focalLength) / distance;
+		const bottomY = this.horizonY - (wallBottomOffset * this.raycaster.focalLength) / distance;
+		return { origin: topY, magnitude: bottomY - topY }
+	}
+
+	private appendGridPoint(
+		gridHit: number,
+		angle: number,
+		index: number,
+		map: GameMapInterface
+	): void {
+		const distance = this.raycaster.removeFishEye(gridHit, angle, map.playerAngle);
+		const y = this.floorPoint(distance);
+		this.batches.addGridPoint({ x: index, y });
+	}
+
+	private floorPoint(distance: number): number {
+		const floorOffset = -Settings.CAMERA_HEIGHT;
+		return this.horizonY - (floorOffset * this.raycaster.focalLength) / distance;
+	}
+
 }
--- app/raycaster/src/game/game.ts
+++ app/raycaster/src/game/game.ts
@@ -2,27 +2,27 @@ import { ContextRendererInterface } from '../renderer/interface';
 import { GameMapInterface } from '../gamemap/interface';
 import { RaycasterInterface } from '../raycaster/interface';
 import { Settings } from '../settings';
-import { ColorName } from './color/color_name';
-import { Coordinates, LineSegment } from '../geometry/interfaces';
 import { BrightnessInterface } from '../brightness/interface';
-import { Batches, BatchedRect } from './batches'
 import { BatchRenderer } from './batchRenderer';
 
 class Game {
 	fieldOfVision: number = Settings.FIELD_OF_VISION;
-	private rays: Float32Array;
-	private static readonly HORIZON_Y = Settings.HORIZON_LINE_RATIO * Settings.CANVAS_HEIGHT;
 	private batchRenderer: BatchRenderer;
 
 	constructor(
 		public map: GameMapInterface,
-		private renderer: ContextRendererInterface,
-		private raycaster: RaycasterInterface,
-		private brightness: BrightnessInterface,
+		renderer: ContextRendererInterface,
+		raycaster: RaycasterInterface,
+		brightness: BrightnessInterface,
 		private readonly displayHUD: Boolean
 	) {
-		this.rays = new Float32Array(Settings.CANVAS_WIDTH);
-		this.batchRenderer = new BatchRenderer(renderer);
+		this.batchRenderer = new BatchRenderer(
+			renderer,
+			raycaster,
+			brightness,
+			Settings.HORIZON_LINE_RATIO * Settings.CANVAS_HEIGHT,
+			Settings.CANVAS_WIDTH
+		);
 	}
 
 	update(): void {
@@ -31,204 +31,14 @@ class Game {
 
 	draw(
 	): void {
-		this.batchRenderer.batchSlices();
+		this.batchRenderer.batchSlices(this.map);
+		if (this.displayHUD) {
+			this.batchRenderer.batchHUD(this.map);
+		}
 		this.batchRenderer.renderSlices();
-		const displaySpecBatches = this.batchRenderData();
-		this.renderFrame(displaySpecBatches);
-		this.drawHUD();
-	}
-
-	private renderFrame(batches: Batches): void {
-		this.renderWalls(batches)
-		this.drawGrid(batches);
-	}
-
-	private drawGrid(batches: Batches): void {
-		this.renderer.fillColor(ColorName.BLUE, 50);
-		batches.gridBatch.forEach(rectSpec => this.renderRect(rectSpec));
-	}
-
-	private renderRect(rectSpec: BatchedRect): void {
-		this.renderer.rect({ x: rectSpec.x, y: rectSpec.y }, rectSpec.width, rectSpec.height);
-	}
-
-	private drawHUD(): void {
-		if (!this.displayHUD) {
-			return;
+		if (this.displayHUD) {
+			this.batchRenderer.renderHUD(this.map);
 		}
-		const batches = this.getHUDBatches();
-		this.renderHUD(batches);
-	}
-
-	private renderHUD(batches: Batches): void {
-		this.renderer.save();
-		this.renderHUDMap(batches);
-		this.renderPlayer();
-		this.renderer.restore()
-	}
-
-	private renderHUDMap(batches: Batches): void {
-		for (const [key, lines] of Object.entries(batches.mapBatches)) {
-			this.renderHUDLines(batches, key, lines);
-		}
-	}
-
-	private renderHUDLines(
-		batches: Batches,
-		key: string,
-		lines: LineSegment[]
-	): void {
-		const { color, intensity: weight } = batches.unpackKey(key)
-		this.renderer.stroke(color);
-		this.renderer.strokeWeight(weight);
-		lines.forEach(line => this.renderer.line(line));
-	}
-
-	private renderPlayer(): void {
-		this.setPlayerContext();
-		this.renderer.ellipse(this.map.playerPosition, 0.2);
-		this.draw2DRays();
-	}
-
-	private setPlayerContext(): void {
-		this.renderer.stroke(ColorName.WHITE);
-		this.renderer.fillColor(ColorName.RED, 100);
-		this.renderer.noStroke();
-	}
-
-	private getHUDBatches(): Batches {
-		const batches = new Batches();
-		batches.addMapWalls(this.map.walls);
-		batches.addMapWalls(this.map.playerTrail);
-		return batches;
-	}
-
-	private batchRenderData(
-	): Batches {
-		this.rays = this.raycaster.getViewRays(this.map.playerAngle);
-		let batches = new Batches();
-		batches = this.appendAllSlices(batches);
-		return batches;
-	}
-
-	private appendAllSlices(
-		batches: Batches,
-	): Batches {
-		for (let i = 0; i < Settings.CANVAS_WIDTH; i++) {
-			batches = this.appendSlice(batches, i);
-		}
-		return batches;
-	}
-
-	private appendSlice(
-		batches: Batches,
-		index: number,
-	): Batches {
-		const angle = this.rays[index];
-		batches = this.appendWallSlice(batches, angle, index);
-		batches = this.appendGridSlice(batches, angle, index);
-		return batches;
-	}
-
-	private appendWallSlice(
-		batches: Batches,
-		angle: number,
-		index: number,
-	): Batches {
-		const { distance, color } = this.getAdjustedDistance(angle);
-		const slice = this.sliceHeight(distance, this.raycaster.focalLength);
-		const wallBrightness = this.brightness.calculateBrightness(distance);
-		batches.addWallSlice(color, wallBrightness, { x: index, y: slice.origin }, slice.magnitude);
-		return batches;
-	}
-
-	private getAdjustedDistance(angle: number): { distance: number, color: ColorName } {
-		const { distance, color } = this.map.castRay(angle, Settings.MAX_DISTANCE);
-		const correctedDistance = this.raycaster.removeFishEye(distance, angle, this.map.playerAngle);
-		return { distance: correctedDistance, color };
-	}
-
-	private appendGridSlice(
-		batches: Batches,
-		angle: number,
-		index: number,
-	): Batches {
-		const { gridHits } = this.map.castRay(angle, Settings.MAX_DISTANCE);
-		for (let j = 0; j < gridHits.length; j++) {
-			batches = this.appendGridPoint(batches, gridHits[j], angle, index);
-		}
-		return batches;
-	}
-
-	private appendGridPoint(
-		batches: Batches,
-		gridHit: number,
-		angle: number,
-		index: number,
-	): Batches {
-		const distance = this.raycaster.removeFishEye(gridHit, angle, this.map.playerAngle);
-		const y = this.floorPoint(distance, this.raycaster.focalLength);
-		batches.addGridPoint({ x: index, y });
-		return batches;
-	}
-
-	private renderWalls(batches: Batches): void {
-		for (const [key, rects] of Object.entries(batches.wallBatches)) {
-			this.unpackAndRender(batches, rects, key);
-		}
-	}
-
-	private unpackAndRender(
-		batches: Batches,
-		rects: BatchedRect[],
-		key: string,
-	): void {
-		const { color, intensity: brightnessValue } = batches.unpackKey(key);
-		this.renderer.fillColor(color, brightnessValue);
-		this.renderRects(rects);
-	}
-
-	private renderRects(rects: BatchedRect[]): void {
-		rects.forEach(rect => this.renderRect(rect));
-	}
-
-	private sliceHeight(
-		distance: number,
-		focalLength: number
-	): { origin: number, magnitude: number } {
-		const wallTopOffset = Settings.WALL_HEIGHT - Settings.CAMERA_HEIGHT;
-		const wallBottomOffset = -Settings.CAMERA_HEIGHT;
-		const topY = Game.HORIZON_Y - (wallTopOffset * focalLength) / distance;
-		const bottomY = Game.HORIZON_Y - (wallBottomOffset * focalLength) / distance;
-		return { origin: topY, magnitude: bottomY - topY }
-	}
-
-	private floorPoint(distance: number, focalLength: number): number {
-		const floorOffset = -Settings.CAMERA_HEIGHT;
-		return Game.HORIZON_Y - (floorOffset * focalLength) / distance;
-	}
-
-	private draw2DRays(): void {
-		this.renderer.stroke(ColorName.GREEN);
-		this.renderer.strokeWeight(0.05);
-		for (let index = 0; index < Settings.CANVAS_WIDTH; index++) {
-			const angle = this.rays[index];
-			const { distance } = this.map.castRay(angle, Settings.MAX_DISTANCE);
-			const hit = this.pointOnTrajectory(this.map.playerPosition, angle, distance);
-			this.renderer.line({ start: this.map.playerPosition, end: hit });
-		}
-	}
-
-	private renderBackround(): void {
-		this.renderer.fillColor(ColorName.BLUE, 0.01);
-		this.renderer.rect({ x: 0, y: 0 }, Settings.CANVAS_WIDTH, Settings.CANVAS_HEIGHT);
-	}
-
-	private pointOnTrajectory(coordinates: Coordinates, angle: number, distance: number): Coordinates {
-		return {
-			x: coordinates.x + Math.cos(angle) * distance,
-			y: coordinates.y + Math.sin(angle) * distance
-		};
 	}
 }
 export { Game };
