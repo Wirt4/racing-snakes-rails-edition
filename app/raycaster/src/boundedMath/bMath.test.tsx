4671f918bc8a79285fe9b49ebbe9e8ab38363507
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-07-16 14:52:18 -0700

    green

--- app/raycaster/src/boundedMath/bMath.test.tsx
+++ app/raycaster/src/boundedMath/bMath.test.tsx
@@ -1,5 +1,5 @@
-import { BMath } from './bMath'
 import { describe, test, expect, } from '@jest/globals';
+import { BMath } from './bmath';
 
 function isRoundedToPlaces(num: number, places: number): boolean {
 	const factor = Math.pow(10, places);
@@ -18,5 +18,36 @@ describe('bMath.cos', () => {
 		const unbounded = Math.cos(Math.PI / 7.5);
 		expect(Math.abs(result - unbounded)).toBeLessThan(1e-7);
 	})
+	test('returns the same value for the same input', () => {
+		const bMath = new BMath(10);
+		const result1 = bMath.cos(Math.PI / 7.5);
+		const result2 = bMath.cos(Math.PI / 7.5);
+		expect(result1).toEqual(result2);
+	});
+})
 
+describe('bMath.sin', () => {
+	test('answer may not be longer than digits return the sine of a number', () => {
+		const bMath = new BMath(10)
+		const result = bMath.sin(Math.PI / 7.5);
+		expect(isRoundedToPlaces(result, 10)).toBe(true);
+	})
+	test('answer may not be longer than digits return the sine of a number', () => {
+		const bMath = new BMath(7)
+		const result = bMath.sin(Math.PI / 7.5);
+		const unbounded = Math.sin(Math.PI / 7.5);
+		expect(Math.abs(result - unbounded)).toBeLessThan(1e-7);
+	})
+	test('returns the same value for the same input', () => {
+		const bMath = new BMath(10);
+		const result1 = bMath.sin(Math.PI / 7.5);
+		const result2 = bMath.sin(Math.PI / 7.5);
+		expect(result1).toEqual(result2);
+	});
+	test('sin is not cos', () => {
+		const bMath = new BMath(10);
+		const result1 = bMath.sin(Math.PI / 7.5);
+		const result2 = bMath.cos(Math.PI / 7.5);
+		expect(result1).not.toEqual(result2);
+	});
 })
--- app/raycaster/src/boundedMath/bmath.ts
+++ app/raycaster/src/boundedMath/bmath.ts
@@ -1,7 +1,30 @@
-export class BMath {
+const cosCache: Map<number, number> = new Map();
+const sinCache: Map<number, number> = new Map();
+class BMath {
 	constructor(private decimalPlaces: number) { }
+
 	cos(x: number): number {
+		const in_x = this.roundToPlaces(x);
+		if (cosCache.has(in_x)) {
+			return cosCache.get(in_x)!;
+		}
+		cosCache.set(in_x, this.roundToPlaces(Math.cos(x)));
+		return cosCache.get(in_x)!;
+	}
+
+	sin(x: number): number {
+		const in_x = this.roundToPlaces(x);
+		if (sinCache.has(in_x)) {
+			return sinCache.get(in_x)!;
+		}
+		sinCache.set(in_x, this.roundToPlaces(Math.sin(x)));
+		return sinCache.get(in_x)!;
+	}
+
+	private roundToPlaces(num: number): number {
 		const factor = Math.pow(10, this.decimalPlaces);
-		return Math.round(Math.cos(x) * factor) / factor;
+		return Math.round(num * factor) / factor;
 	}
 }
+
+export { BMath }
