31283eb5c058505bdf354ea81e188122c8ac6f27
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-07-18 00:02:55 -0700

    red

--- app/raycaster/src/listener/window_listener.test.tsx
+++ app/raycaster/src/listener/window_listener.test.tsx
@@ -3,7 +3,7 @@ import { describe, test, expect, jest } from '@jest/globals';
 import { Directions } from '../controls/directions';
 
 describe('Keydown Tests', () => {
-	test('should handle keydown events', () => {
+	test('should turn left', () => {
 		const mockWorker = { postMessage: jest.fn() };
 		const listener = new Listener(mockWorker as unknown as Worker);
 		const keyStroke = 'ArrowLeft';
@@ -15,4 +15,17 @@ describe('Keydown Tests', () => {
 			direction: Directions.LEFT
 		});
 	})
+	test('if user hits the same key twice, just call the worker post once', () => {
+		const mockWorker = { postMessage: jest.fn() };
+		const listener = new Listener(mockWorker as unknown as Worker);
+		const keyStroke = 'ArrowLeft';
+
+		listener.keydown(keyStroke);
+		listener.keydown(keyStroke);
+		expect(mockWorker.postMessage.mock.calls).toEqual([
+			{ type: 'turn', direction: Directions.LEFT }
+		]);
+
+	})
+
 })
