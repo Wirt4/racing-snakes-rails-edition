cfac5d98c28e002c2962558822b7c12a512c3ff0
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-07-18 01:01:28 -0700

    one bigass cleanup

--- app/raycaster/src/main.ts
+++ app/raycaster/src/main.ts
@@ -1,6 +1,7 @@
 // test for game loop
 import { Settings } from './settings';
 import { Directions } from './controls/directions';
+import { Listener } from './listener/listener';
 const canvas = document.createElement("canvas");
 const width = Settings.CANVAS_WIDTH;
 canvas.width = width;
@@ -18,35 +19,12 @@ worker.postMessage({
 	settings: Settings
 }, [offscreen]);
 
-let lastDirection: Directions | null = null;
-
-function postIfDirectionChanged(direction: Directions | null) {
-	if (direction !== lastDirection) {
-		worker.postMessage({ type: "turn", direction });
-		lastDirection = direction;
-	}
-}
+const listener = new Listener(worker);
 
 window.addEventListener("keydown", (e: KeyboardEvent) => {
-	let direction: Directions | null = null;
-	if (e.key === "ArrowLeft") direction = Directions.LEFT;
-	if (e.key === "ArrowRight") direction = Directions.RIGHT;
-	postIfDirectionChanged(direction);
+	listener.keydown(e.key);
 });
 
-function IsReleasingSameKey(keyStroke: string): boolean {
-	if (keyStroke == "ArrowLeft" && lastDirection === Directions.LEFT) {
-		return true
-	}
-	return keyStroke === "ArrowRight" && lastDirection === Directions.RIGHT
-}
-
-function resetDirection() {
-	lastDirection = null;
-}
-
 window.addEventListener("keyup", (e: KeyboardEvent) => {
-	if (IsReleasingSameKey(e.key)) {
-		resetDirection()
-	}
+	listener.keyup(e.key);
 });
