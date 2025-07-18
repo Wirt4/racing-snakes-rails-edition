3c604f338f35ce405ebd0f164b166ae440018a33
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-07-17 22:28:07 -0700

    removed magic number

--- app/raycaster/src/color_key/color_key_cache.ts
+++ app/raycaster/src/color_key/color_key_cache.ts
@@ -8,8 +8,8 @@ export interface ColorKey {
 const keyCache = new Map<ColorName, Map<number, ColorKey>>();
 
 export function getColorKey(color: ColorName, intensity: number): ColorKey {
-
-	const rounded_key = Math.round(intensity * 16);
+	const levels = 16;
+	const rounded_key = Math.round(intensity * levels);
 
 	if (!keyCache.has(color)) {
 		keyCache.set(color, new Map());
@@ -17,7 +17,7 @@ export function getColorKey(color: ColorName, intensity: number): ColorKey {
 	const innerMap = keyCache.get(color)!;
 
 	if (!innerMap.has(rounded_key)) {
-		innerMap.set(rounded_key, { color, intensity: rounded_key / 16 });
+		innerMap.set(rounded_key, { color, intensity: rounded_key / levels });
 	}
 	return innerMap.get(rounded_key)!;
 }
