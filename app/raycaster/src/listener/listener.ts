39cfbef9670a8d3c00b031e5e592a772922c1bb4
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-07-18 00:24:36 -0700

    green

--- app/raycaster/src/listener/listener.ts
+++ app/raycaster/src/listener/listener.ts
@@ -13,9 +13,16 @@ class Listener {
 	constructor(private worker: Worker) { }
 
 	keydown(keystroke: string): void {
+		if (!this.isValidKey(keystroke)) {
+			return;
+		}
 		const direction = keystroke === "ArrowLeft" ? LastDirection.LEFT : LastDirection.RIGHT;
 		this.postIfDirectionChanged(direction);
 		this.lastDirection = direction;
+	}
+
+	private isValidKey(keystroke: string): boolean {
+		return keystroke === "ArrowLeft" || keystroke === "ArrowRight";
 	}
 
 	private postIfDirectionChanged(direction: LastDirection): void {
