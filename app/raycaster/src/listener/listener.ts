27f8d3bb84aa243aa1c481d2a21480bf3798e3d2
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-07-18 00:06:44 -0700

    refactor

--- app/raycaster/src/listener/listener.ts
+++ app/raycaster/src/listener/listener.ts
@@ -1,11 +1,14 @@
 import { Directions } from '../controls/directions';
+
+const type = "turn";
+
 class Listener {
 	private buttonPressed: boolean = false;
 	constructor(private worker: Worker) { }
 
 	keydown(keystroke: string): void {
 		if (!this.buttonPressed) {
-			this.worker.postMessage({ type: "turn", direction: Directions.LEFT });
+			this.worker.postMessage({ type, direction: Directions.LEFT });
 		}
 		this.buttonPressed = true;
 	}
