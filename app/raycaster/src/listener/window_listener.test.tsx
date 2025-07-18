8dd9a22f00e139f790611b43b669fce3f41ccca5
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-07-18 00:48:51 -0700

    red

--- app/raycaster/src/listener/window_listener.test.tsx
+++ app/raycaster/src/listener/window_listener.test.tsx
@@ -1,11 +1,17 @@
 import { Listener } from './listener';
-import { describe, test, expect, jest } from '@jest/globals';
+import { beforeEach, describe, test, expect, jest } from '@jest/globals';
 import { Directions } from '../controls/directions';
 
 describe('Keydown Tests', () => {
+	let mockWorker: Worker;
+	let listener: Listener;
+	let postMessageSpy: any;
+	beforeEach(() => {
+		mockWorker = { postMessage: jest.fn() } as unknown as Worker;
+		listener = new Listener(mockWorker);
+		postMessageSpy = jest.spyOn(mockWorker, 'postMessage');;
+	});
 	test('should turn left', () => {
-		const mockWorker = { postMessage: jest.fn() };
-		const listener = new Listener(mockWorker as unknown as Worker);
 		const keyStroke = 'ArrowLeft';
 
 		listener.keydown(keyStroke);
@@ -15,22 +21,19 @@ describe('Keydown Tests', () => {
 			direction: Directions.LEFT
 		});
 	})
+
 	test('if user hits the same key twice, just call the worker post once', () => {
-		const mockWorker = { postMessage: jest.fn() };
-		const listener = new Listener(mockWorker as unknown as Worker);
 		const keyStroke = 'ArrowLeft';
 
 		listener.keydown(keyStroke);
 		listener.keydown(keyStroke);
 
-		expect(mockWorker.postMessage.mock.calls).toEqual([
+		expect(postMessageSpy.mock.calls).toEqual([
 			[{ type: 'turn', direction: Directions.LEFT }]
 		]);
 	})
 
 	test('should turn right', () => {
-		const mockWorker = { postMessage: jest.fn() };
-		const listener = new Listener(mockWorker as unknown as Worker);
 		const keyStroke = 'ArrowRight';
 
 		listener.keydown(keyStroke);
@@ -42,43 +45,39 @@ describe('Keydown Tests', () => {
 	})
 
 	test('should turn right after turning left', () => {
-		const mockWorker = { postMessage: jest.fn() };
-		const listener = new Listener(mockWorker as unknown as Worker);
-
 		listener.keydown('ArrowLeft');
 		listener.keydown('ArrowRight');
 
-		expect(mockWorker.postMessage.mock.calls).toEqual([
+		expect(postMessageSpy.mock.calls).toEqual([
 			[{ type: 'turn', direction: Directions.LEFT }],
 			[{ type: 'turn', direction: Directions.RIGHT }]
 		]);
 	})
 	test('should not turn if the same key is pressed twice', () => {
-		const mockWorker = { postMessage: jest.fn() };
-		const listener = new Listener(mockWorker as unknown as Worker);
-
 		listener.keydown('ArrowLeft');
 		listener.keydown('ArrowLeft');
 
-		expect(mockWorker.postMessage.mock.calls).toEqual([
+		expect(postMessageSpy.mock.calls).toEqual([
 			[{ type: 'turn', direction: Directions.LEFT }]
 		]);
 	})
 	test('shoult not accept any keys other than ArrowLeft or ArrowRight', () => {
-		const mockWorker = { postMessage: jest.fn() };
-		const listener = new Listener(mockWorker as unknown as Worker);
-
 		listener.keydown('a');
 
 		expect(mockWorker.postMessage).not.toHaveBeenCalled();
 	})
 	test('can turn left again after releasing arrow left key', () => {
-		const mockWorker = { postMessage: jest.fn() };
-		const listener = new Listener(mockWorker as unknown as Worker);
 		const keyStroke = 'ArrowLeft';
 		listener.keydown(keyStroke);
 		listener.keyup(keyStroke);
 		listener.keydown(keyStroke);
 		expect(mockWorker.postMessage).toHaveBeenCalledTimes(2);
 	})
+	test('the keyups need to match', () => {
+		listener.keydown('ArrowLeft');
+		listener.keyup('ArrowRight');
+		listener.keydown('ArrowLeft');
+		expect(mockWorker.postMessage).toHaveBeenCalledTimes(1);
+	})
+
 })
