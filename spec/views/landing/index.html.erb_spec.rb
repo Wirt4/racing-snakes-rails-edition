7e9fd37a697eb780d563f5a34b7fa6dc1d8ae104
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-07-21 17:13:00 -0700

    green

--- spec/views/landing/index.html.erb_spec.rb
+++ spec/views/landing/index.html.erb_spec.rb
@@ -4,7 +4,10 @@ require 'rails_helper'
 
 RSpec.describe 'landing/index.html.erb', type: :view do
   it 'shows the sign up link' do
+    assign(:game, [Game.new(id: 1, title: 'Test Game')])
+
     render
+
     expect(rendered).to include('Play Now!')
     expect(rendered).to include('href="/games/1"')
   end
