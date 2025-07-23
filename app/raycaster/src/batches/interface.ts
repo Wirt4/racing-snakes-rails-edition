b900e3807a383435b4a1001eb83e2a3734d77716
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-07-22 22:00:21 -0700

    removed unnecssary method from interface

--- app/raycaster/src/batches/interface.ts
+++ app/raycaster/src/batches/interface.ts
@@ -2,13 +2,13 @@ import { ColorName } from '../color/color_name';
 import { Coordinates } from '../geometry/interfaces';
 import { WallInterface } from '../gamemap/interface';
 import { BatchedRect } from './rectInterface';
+
 interface BatchesInterface {
 	clear(): void;
 	addWallSlice(color: ColorName, brightness: number, x: number, y: number, height: number): void;
 	releaseSlice(rect: BatchedRect): void;
 	addGridPoint(origin: Coordinates): void;
 	addMapWalls(walls: Array<WallInterface>): void;
-	addMapWall(wall: WallInterface): void;
 }
 
 
