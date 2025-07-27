d7099ed4a92f2429825c13522a709206d3d11c6d
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-07-26 22:37:07 -0700

    refactor

--- app/raycaster/src/listener/listener.ts
+++ app/raycaster/src/listener/listener.ts
@@ -10,18 +10,20 @@ interface ListenerInterface {
 
 class Listener implements ListenerInterface {
 	private lastDirection: Directions = Directions.NONE;
-	private leftKey = "ArrowLeft";
-	constructor(private worker: Worker, private keyMap: KeyMapInterface) { }
+	constructor(
+		private worker: Worker,
+		private keyMap: KeyMapInterface
+	) { }
 
 	keydown(keystroke: string): void {
-		if (!this.isValidKey(keystroke)) {
+		if (!this.keyMap.isMappedKey(keystroke)) {
 			return;
 		}
 		this.handleValidKey(keystroke);
 	}
 
 	keyup(keystroke: string): void {
-		if (!(this.isValidKey(keystroke) && this.isLastDirection(keystroke))) {
+		if (!(this.keyMap.isMappedKey(keystroke) && this.isLastDirection(keystroke))) {
 			return;
 		}
 		this.lastDirection = Directions.NONE;
@@ -29,14 +31,6 @@ class Listener implements ListenerInterface {
 
 	private isLastDirection(keystroke: string): boolean {
 		return this.lastDirection === this.keyMap.toDirection(keystroke);
-	}
-
-	private keystrokeToDirection(keystroke: string): Directions {
-		return this.leftKey === keystroke ? Directions.LEFT : Directions.RIGHT;
-	}
-
-	private isValidKey(keystroke: string): boolean {
-		return this.keyMap.isMappedKey(keystroke);
 	}
 
 	private handleValidKey(keystroke: string): void {
