f50e69b8718a69fdfa786ae628202d834fcfecce
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-07-26 18:43:43 -0700

    moved bootstrap setup to separate function

--- app/raycaster/src/bootstrap/bootstrap.ts
+++ app/raycaster/src/bootstrap/bootstrap.ts
@@ -1,4 +1,41 @@
-function bootstrap() {
-	throw new Error('Bootstrap function is not implemented yet.')
+
+import { Settings } from '../settings/settings';
+import { Listener } from '../listener/listener';
+
+export function bootstrap({
+	canvasId,
+	workerPath,
+	settings,
+}: {
+	canvasId: string;
+	workerPath: string;
+	settings: Settings;
+}): void {
+	const canvas = document.createElement("canvas");
+	canvas.width = settings.CANVAS_WIDTH;
+	canvas.height = settings.CANVAS_HEIGHT;
+	canvas.id = canvasId;
+	document.getElementById("app")?.appendChild(canvas);
+
+	const offscreen = canvas.transferControlToOffscreen();
+	const worker = new Worker(workerPath, { type: 'module' });
+
+	worker.postMessage(
+		{
+			type: "init",
+			canvas: offscreen,
+			settings,
+		},
+		[offscreen]
+	);
+
+	const listener = new Listener(worker);
+
+	window.addEventListener("keydown", (e: KeyboardEvent) => {
+		listener.keydown(e.key);
+	});
+
+	window.addEventListener("keyup", (e: KeyboardEvent) => {
+		listener.keyup(e.key);
+	});
 }
-export { bootstrap };
--- app/raycaster/src/main.ts
+++ app/raycaster/src/main.ts
@@ -1,30 +1,36 @@
 import { Settings } from './settings/settings';
 import { Listener } from './listener/listener';
-
-const canvas = document.createElement("canvas");
+import { bootstrap } from './bootstrap/bootstrap';
+//const canvas = document.createElement("canvas");
 const settings = new Settings();
-const width = settings.CANVAS_WIDTH;
-canvas.width = width;
-canvas.height = settings.CANVAS_HEIGHT;
-canvas.id = settings.CANVAS_ID;
-document.getElementById(settings.HTML_ELEMENT_ID)?.appendChild(canvas);
-
-const offscreen = canvas.transferControlToOffscreen();
-const addressToCompiledExecutable = '/workers/worker.js'
-const worker = new Worker(addressToCompiledExecutable, { type: 'module' });
-
-worker.postMessage({
-	type: "init",
-	canvas: offscreen,
-	settings
-}, [offscreen]);
-
-const listener = new Listener(worker);
-
-window.addEventListener("keydown", (e: KeyboardEvent) => {
-	listener.keydown(e.key);
+bootstrap({
+	canvasId: "game-window",
+	workerPath: "/workers/worker.js",
+	settings,
 });
 
-window.addEventListener("keyup", (e: KeyboardEvent) => {
-	listener.keyup(e.key);
-});
+// const width = settings.CANVAS_WIDTH;
+// canvas.width = width;
+// canvas.height = settings.CANVAS_HEIGHT;
+// canvas.id = settings.CANVAS_ID;
+// document.getElementById(settings.HTML_ELEMENT_ID)?.appendChild(canvas);
+//
+// const offscreen = canvas.transferControlToOffscreen();
+// const addressToCompiledExecutable = '/workers/worker.js'
+// const worker = new Worker(addressToCompiledExecutable, { type: 'module' });
+//
+// worker.postMessage({
+// 	type: "init",
+// 	canvas: offscreen,
+// 	settings
+// }, [offscreen]);
+//
+// const listener = new Listener(worker);
+//
+// window.addEventListener("keydown", (e: KeyboardEvent) => {
+// 	listener.keydown(e.key);
+// });
+//
+// window.addEventListener("keyup", (e: KeyboardEvent) => {
+// 	listener.keyup(e.key);
+// });
