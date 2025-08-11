3b5f371ceb4a4f6e019387a63e9a18ed53cd2654
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-08-11 15:39:48 -0700

    stated goals for this branch have been reached, tidy, clean up then open the castRay issue in a separate branch

--- app/raycaster/src/gamemap/interface.ts
+++ app/raycaster/src/gamemap/interface.ts
@@ -1,20 +1,14 @@
-import { Coordinates, LineSegment } from '../geometry/interfaces';
 import { PlayerInterface } from '../player/interface';
 import { WallInterface } from '../wall/interface'
 import { Slice } from '../slice/interface'
 import { ArenaInterface } from '../arena/interface';
 
 export interface GameMapInterface {
-	//gridlines and boundarys should belong to arena
 	arena: ArenaInterface;
-	// trails should belong to each player
 	walls: WallInterface[];
-
 	player: PlayerInterface;
-	//castRay should be in it's own class -- gameMap interface should have no knowlege of it
 	castRay(angle: number, distance: number): Slice;
 	resetIntersections(): void;
-	//appendWall makes no sense --- should remove
 	appendWall(wall: WallInterface): void;
 }
 
