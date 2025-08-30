78afb6139a6b0f13ff7f490de929e0c8294c2c2f
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-08-30 13:00:29 -0700

    basic throughline for controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,4 +4,21 @@ class GamesController < ApplicationController
   def show
     # dummy object or Game.find(params[:id])
   end
+
+  # method
+  def user_prompt_controls
+    # information hidden
+    # any detection of user agent or device type
+    # preconditions
+    # --The app is running
+    # --the device_detection gem is installed
+    # postconditions
+    #   The string is of one of two states:
+    #    message for desktop-based
+    #    message for tablet based controls
+    #  # inputs: none
+    # outputs: returns a string
+    'Test Controller'
+  end
+  helper_method :user_prompt_controls
 end
