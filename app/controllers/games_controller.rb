9858d54b94438af8db0d96c47a326c6ca73e9998
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-08-30 13:26:40 -0700

    basic functionality

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# reqiure the device detector
+require 'device_detector'
 class GamesController < ApplicationController
   def show
     # dummy object or Game.find(params[:id])
@@ -18,13 +20,16 @@ class GamesController < ApplicationController
     #    message for tablet based controls
     #  # inputs: none
     #  #get the user agent
-    request.user_agent
+    user_agent = request.user_agent
     # expect the user agent string displays as the output
     #  # create a device detector object from the user agent
+    client = DeviceDetector.new(user_agent)
     #  # create a result message that defaults to mobile
-
+    message = 'Tap to turn'
     #  # if the device type is desktop, set the result to a message for arrow keys
+    message = 'Use arrow keys to turn' if client.device_type == 'desktop'
     # return the result message
+    message
   end
   helper_method :user_prompt_controls
 end
