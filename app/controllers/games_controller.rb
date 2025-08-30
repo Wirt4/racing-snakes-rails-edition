40e0bfb29dfc3d942da22cff47fdd6463c2541b2
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-08-30 13:15:14 -0700

    installed device detector

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -17,8 +17,14 @@ class GamesController < ApplicationController
     #    message for desktop-based
     #    message for tablet based controls
     #  # inputs: none
-    # outputs: returns a string
-    'Test Controller'
+    #  #get the user agent
+    request.user_agent
+    # expect the user agent string displays as the output
+    #  # create a device detector object from the user agent
+    #  # create a result message that defaults to mobile
+
+    #  # if the device type is desktop, set the result to a message for arrow keys
+    # return the result message
   end
   helper_method :user_prompt_controls
 end
