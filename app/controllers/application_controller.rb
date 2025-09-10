91c6b6f75601b210bbc312439a96b45bfe84eaa7
Wirt S <34081557+Wirt4@users.noreply.github.com>
2025-09-09 17:43:45 -0700

    green

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
-# frozen_string_literal: true
-
 class ApplicationController < ActionController::Base
   def current_user
-    super
+    create_guest_user
+  end
+
+  private
+
+  def create_guest_user
+    u = User.create(password: 'foobar', email: "guest_#{Time.now.to_i}#{rand(99)}@example.com")
+    u.save(validate: false)
+    u
   end
 end
--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -7,21 +7,28 @@ RSpec.describe ApplicationController, type: :controller do
       render plain: 'OK'
     end
   end
+  # context: when user is logged in
   # mock the user
   let(:user) { User.create!(password: 'foobarbar', email: 'test@example.com') }
-  # fake a signed in user for next test
-  before do
-    allow(controller).to receive(:current_user).and_return(user)
+
+  context 'when user is logged in' do
+    # fake a signed in user for next test
+    before do
+      allow(controller).to receive(:current_user).and_return(user)
+    end
+
+    it 'returns the logged-in user' do
+      expect(controller.current_user).to eq(user)
+    end
   end
 
-  it 'returns the logged-in user' do
-    expect(controller.current_user).to eq(user)
-  end
-
-  it 'if not logged in, then creates guest user' do
-    # know it's creating a guest user
-    expect do
-      controller.send(:current_user)
-    end.to change(User, :count).by(1)
+  context 'user is not logged in' do
+    # no current user mocked here, user is logged out
+    it 'if not logged in, then creates guest user' do
+      # know it's creating a guest user
+      expect do
+        controller.send(:current_user)
+      end.to change(User, :count).by(1)
+    end
   end
 end
